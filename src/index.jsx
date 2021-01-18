5768818de226d1596eef6306b7b8a0d1b6438009
Alisher Ortiqov <mcpeblocker@gmail.com>
2021-01-18 06:51:42 +0500

    initial

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import {Button, TextView, contentView, WebView} from 'tabris';
+
+contentView.append(
+    <WebView
+      left={0}
+      right={0}
+      top={0}
+      bottom={0}
+      url="http://pskhiva.uz"
+    />
+);
